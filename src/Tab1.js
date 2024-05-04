ba0fbdeee87d4e3f9d9c1ac12ea38cfa949d90ff
GhiathAlshammaa <g.alshammaa@rewe-group.at>
2024-05-04 15:19:27 +0200

    fix the sub tabs bar behavior

--- src/Tab1.js
+++ src/Tab1.js
@@ -1,47 +1,80 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import Creation from './tab1/Creation'; 
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import Creation from './tab1/Creation';
 
-const tabs = ['Creation', 'Modify', 'Preview PDF', 'Generate PDF'];
+const tabs = ['Test details', 'Test Questions', 'Preview PDF', 'Generate PDF'];
 
 const Tab1 = () => {
   const [activePoint, setActivePoint] = useState(tabs[0]);
-
-  const goLeft = () => {
-    const currentIndex = tabs.indexOf(activePoint);
-    if (currentIndex > 0) {
-      setActivePoint(tabs[currentIndex - 1]);
-    }
-  };
+  const [visitedPoints, setVisitedPoints] = useState([]);
 
   const goRight = () => {
     const currentIndex = tabs.indexOf(activePoint);
     if (currentIndex < tabs.length - 1) {
-      setActivePoint(tabs[currentIndex + 1]);
+      if (!visitedPoints.includes(activePoint)) {
+        setVisitedPoints((prevVisitedPoints) => [
+          ...prevVisitedPoints,
+          activePoint,
+        ]);
+      }
+      const nextPoint = tabs[currentIndex + 1];
+      setActivePoint(nextPoint);
+    }
+  };
+
+  const goLeft = () => {
+    const currentIndex = tabs.indexOf(activePoint);
+    if (currentIndex > 0) {
+      if (!visitedPoints.includes(activePoint)) {
+        setVisitedPoints((prevVisitedPoints) => [
+          ...prevVisitedPoints,
+          activePoint,
+        ]);
+      }
+      setActivePoint(tabs[currentIndex - 1]);
     }
   };
 
   return (
     <>
       <div className="progress-container">
-        <div className="arrow left-arrow" onClick={goLeft}><FontAwesomeIcon icon={faChevronLeft} /></div>
+        <div className="arrow left-arrow" onClick={goLeft}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </div>
         <div className="progress-line">
-          {tabs.map(point => (
+          {tabs.map((point) => (
             <div
               key={point}
-              className={`progress-step ${activePoint === point ? 'active' : ''}`}
-              onClick={() => setActivePoint(point)}
+              className={`progress-step ${
+                activePoint === point ? 'active' : ''
+              }`}
+              onClick={() => {
+                if (!visitedPoints.includes(activePoint)) {
+                  setVisitedPoints((prevVisitedPoints) => [
+                    ...prevVisitedPoints,
+                    activePoint,
+                  ]);
+                }
+                setActivePoint(point);
+              }}
             >
-              {activePoint === point && <div>{point}</div>}
+              {(activePoint === point || visitedPoints.includes(point)) && (
+                <div>{point}</div>
+              )}
             </div>
           ))}
         </div>
-        <div className="arrow right-arrow" onClick={goRight}><FontAwesomeIcon icon={faChevronRight} /></div>
+        <div className="arrow right-arrow" onClick={goRight}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
       </div>
-      {activePoint === 'Creation' && <Creation />}
+      {activePoint === 'Test details' && <Creation />}
     </>
   );
-}
+};
 
 export default Tab1;
