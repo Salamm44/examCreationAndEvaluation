2184f150524e7675b8429691529ce59415bdce74
GhiathAlshammaa <g.alshammaa@rewe-group.at>
2024-05-12 09:30:00 +0200

    fix issue with webpack

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  // ...
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.mjs$/,
         enforce: 'pre',
         use: ['source-map-loader'],
-        exclude: /node_modules\/@react-aria\/ssr/, 
+        exclude: /node_modules/,
       },
     ],
   },
-  // ...
+  resolve: {
+    alias: {
+      '@react-aria/ssr': false,
+    },
+  },
 };
